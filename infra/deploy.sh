e75f8576286b3b4525734109a05e1b96728d083a
cunicopia <112027862+cunicopia-dev@users.noreply.github.com>
2025-06-01 13:31:27 -0400

    Update deploy.sh

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -6,21 +6,19 @@ set -e
 
 # Check for required parameters
 if [ "$#" -lt 4 ]; then
-    echo "Usage: $0 --vpc-id <vpc-id> --subnet-id <subnet-id> [--postgres] [--instance-type t4g.small|t4g.medium]"
+    echo "Usage: $0 --vpc-id <vpc-id> --subnet-id <subnet-id> [--instance-type t4g.small|t4g.medium]"
     exit 1
 fi
 
 # Parse arguments
 VPC_ID=""
 SUBNET_ID=""
-USE_POSTGRES="false"
 INSTANCE_TYPE="t4g.small"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         --vpc-id) VPC_ID="$2"; shift 2 ;;
         --subnet-id) SUBNET_ID="$2"; shift 2 ;;
-        --postgres) USE_POSTGRES="true"; shift ;;
         --instance-type) INSTANCE_TYPE="$2"; shift 2 ;;
         *) echo "Unknown parameter: $1"; exit 1 ;;
     esac
@@ -63,8 +61,7 @@ aws cloudformation deploy \
         SubnetId="$SUBNET_ID" \
         InstanceType="$INSTANCE_TYPE" \
         InstanceProfileName="$INSTANCE_PROFILE" \
-        S3BucketName="$S3_BUCKET_NAME" \
-        UsePostgres="$USE_POSTGRES"
+        S3BucketName="$S3_BUCKET_NAME"
 
 # Get instance info
 ASG_NAME=$(aws cloudformation describe-stacks --stack-name n8n-ec2 --query 'Stacks[0].Outputs[?OutputKey==`AutoScalingGroupName`].OutputValue' --output text)
